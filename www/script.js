4fa9e37c79eb5a3f0bbe791540d5530f7ed2a08f
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2025-11-24 20:00:30 +0100

    Front creado (maomeno)

--- www/script.js
+++ www/script.js
@@ -2,13 +2,11 @@
     data.forEach(category => {
       let parent = document.getElementsByTagName('ul')[0]
       let child = document.createElement('li')
-      // child.innerText = JSON.stringify(category)
       child.innerText = category.name
       parent.appendChild(child)
     })
   }*/ //Poner categorias en una lista
   import Swal from 'sweetalert2'
-  
 
   fetch("http://localhost:3000/categories")
     .then(res => res.json())
