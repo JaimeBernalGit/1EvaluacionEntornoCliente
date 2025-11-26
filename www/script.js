9f331214ae44d5a16e837c8466e6616c2278b844
unknown <jaimebernalgiagnotti@gmail.com>
2025-11-26 17:14:25 +0100

    Borrar categorías

--- www/script.js
+++ www/script.js
@@ -7,7 +7,14 @@ let drawData = (data) => {
       let button = document.createElement("button")
       button.innerText = category.name
       button.classList.add("category-btn")
+      let deleteBtn = document.createElement("button")
+      deleteBtn.innerHTML = "Borrar " + category.name
+      deleteBtn.classList.add("delete-btn")
+      deleteBtn.onclick = () => {
+        deleteCategory(category.id);
+      }
       li.appendChild(button)
+      li.appendChild(deleteBtn)
       parent.appendChild(li)
     })
   }
@@ -50,7 +57,6 @@ boton_categoria_ok.addEventListener('click', (e) => {
     },
     body: JSON.stringify(categoryData)
   })
-  .then(response => response.json())
   .then( function (){
     modal_categoria.style.display = 'none';
     modal_categoria.querySelector("#categoryName").value = '';
@@ -70,3 +76,38 @@ boton_categoria_ok.addEventListener('click', (e) => {
     });
   });
 });
+
+//Borrar categorías
+function deleteCategory(id) {
+  Swal.fire({
+  title: "¿Estás seguro de querer borrarla?",
+  text: "Este cambio no se puede revertir",
+  icon: "warning",
+  showCancelButton: true,
+  confirmButtonColor: "#3085d6",
+  cancelButtonColor: "#d33",
+  confirmButtonText: "Borrar"
+}).then((result) => {
+  if (result.isConfirmed) {
+    fetch("http://localhost:3000/categories/" + id, {
+    method: 'DELETE'
+    })
+    .then(() => {
+      fetch("http://localhost:3000/categories")
+        .then(res => res.json())
+        .then(data => drawData(data));
+    })
+    .catch(error => {
+      console.error('Error:', error);
+       Swal.fire({
+      icon: "error",
+      title: "Error al borrar la categoría",
+      });
+    });
+    Swal.fire({
+      title: "Categoría borrada",
+      icon: "success"
+    });
+  }
+});
+}
